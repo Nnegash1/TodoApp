26bfcbccddc67c72b82c5e63addaa904ceefdeb3
Nahom Negash <nahom.n@ravebizz.com>
2022-11-20 12:51:02 -0500

    Bug fix.

--- app/src/main/java/com/example/todoapp/EditToDoScreen.kt
+++ app/src/main/java/com/example/todoapp/EditToDoScreen.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -18,11 +19,11 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class EditToDoScreen : Fragment() {
 
-    private lateinit var binding: FragmentEditToDoScreenBinding
 
     @Inject
     lateinit var factory: LoginViewModelFactory
     private val args by navArgs<EditToDoScreenArgs>()
+    private lateinit var binding: FragmentEditToDoScreenBinding
     private lateinit var todo: TodoApp
     private val todoVm: TodoViewModel by activityViewModels { factory }
 
@@ -32,31 +33,42 @@ class EditToDoScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        todoVm.todo.observe(viewLifecycleOwner) {
-            setInput(it)
-        }
 
-        binding.addTodo.setOnClickListener {
-            todoVm.todo.value = getInput()
-            todoVm.setTodoAtIndex(args.index)
-            findNavController().popBackStack()
-        }
+        todoVm.todo.observe(viewLifecycleOwner) { setInput(it) }
+
+        binding.addTodo.setOnClickListener { getInput() }
 
         binding.deleteBtn.setOnClickListener {
             todoVm.getTodoAtIndex(args.index)
             findNavController().popBackStack()
         }
+
         todo = todoVm.localTodoList[args.index]
+
         setInput(todo)
     }
 
-    private fun getInput(): TodoApp {
-        return with(binding) {
-            TodoApp(
-                todoTitle.text.toString(),
-                todoDetail.text.toString(),
-                complete.isChecked
-            )
+    private fun getInput() {
+        var title: String
+        var body: String
+        var checked: Boolean
+
+        with(binding) {
+            title = todoTitle.text.toString()
+            body = todoDetail.text.toString()
+            checked = complete.isChecked
+        }
+
+        if (!(title.isBlank() || body.isBlank())) {
+            todoVm.todo.value = TodoApp(title, body, checked)
+            todoVm.setTodoAtIndex(args.index)
+            findNavController().popBackStack()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Please fill out all the fields !",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
